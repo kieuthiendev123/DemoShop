034be617dd0d86b52ab638ec3f78cecace82d5cc
BeoDz <kieuthien.dev123@gmail.com>
2019-08-14 11:05:54 -0700

    lession1

---
+++ DemoShop.Data/Interfaces/IHasOwner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoShop.Data.Interfaces
+{
+    public interface IHasOwner<T>
+    {
+        T OwnerId { set; get; }
+
+
+    }
+}
---
+++ DemoShop.Data/Interfaces/ISwitchable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DemoShop.Data.Enums;
+
+namespace DemoShop.Data.Interfaces
+{
+    public interface ISwitchable
+    {
+        Status Status { set; get; }
+    }
+}
