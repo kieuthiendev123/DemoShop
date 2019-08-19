a90cf9fb38b920160e5fb2c9223b28b20f992dfb
BeoDz <kieuthien.dev123@gmail.com>
2019-08-19 11:17:53 -0700

    day2

---
+++ DemoShop.Data.EF/Configgurations/TagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DemoShop.Data.EF.Extensions;
+using DemoShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoShop.Data.EF.Configurations
+{
+    public class TagConfiguration : DbEntityConfiguration<Tag>
+    {
+        public override void Configure(EntityTypeBuilder<Tag> entity)
+        {
+            entity.Property(c => c.Id).HasMaxLength(50)
+                .IsRequired().IsUnicode(false).HasMaxLength(50);
+        }
+    }
+}
---
+++ DemoShop.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoShop.Infrastructure.SharedKernel
+{
+    // đây là class base
+    public abstract class DomainEntity<T>
+    // T là generic động
+    {
+        public T Id { get; set; }
+        /// <summary>
+        ///  true if domain has identity
+        /// </summary>
+        /// <returns></returns>
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }   
+
+
+    }
+}
